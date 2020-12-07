b0d4ad2fc4a4a9dfb2d176d1c62595125ae9ad08
Sabrina Button <43886209+sabrinabutton@users.noreply.github.com>
2020-12-07 16:43:52 -0500

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,22 @@
+/********************************************************************************  
+*									      													
+*     HOLIDAY HACKS EXAMPLE
+*
+*     Here's an example project written in C++! It outputs an ASCII
+*     snowman (pretty fun, right?) This is just an idea to get you 
+*     started -- but something like this would be a great entry!
+*     Feel free to delete this when you're feeling comfortable.
+*
+*********************************************************************************/  
+
+#include <iostream>
+
+int main{
+  cout <<"     __ "<< endl;
+  cout <<"   _|==|_"  << endl;
+  cout <<"    (\'\')___/" << endl;
+  cout <<">--(`^^\')" << endl;
+  cout <<"  (`^'^\'`)" << endl;
+  cout <<"  `======\' "<< endl;
+  getchar();
+}
